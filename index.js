6eed3c6d0366d084e633962e754dbfe592ee650d
Noah Hanover <noahsahar@gmail.com>
2016-10-09 17:25:01 -0400

    Last

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
+//var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
-//var databaseUri = 'mongodb://admin:Cyoteam1532@ds053206.mlab.com:53206/gnnhs-server';
+var databaseUri = 'mongodb://admin:Cyoteam1532@ds053206.mlab.com:53206/gnnhs-server';
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
