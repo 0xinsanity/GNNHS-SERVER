d7e4edfd5e62ebaa2107df3a46946dda37c1bd99
Noah Hanover <noahsahar@gmail.com>
2016-10-09 15:52:39 -0400

    YAYAYAYAY

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
+// Example express application adding the parse-server module to expose Parse
+// compatible API routes.
+
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = 'mongodb://admin:Cyoteam1532@ds053206.mlab.com:53206/gnnhs-server';
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://admin:Cyoteam1532@ds053206.mlab.com:53206/gnnhs-server',
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || '5LA5kojEziCljqw4vZ9iZGNRQXwaOJG0jmcQaLYY',
   masterKey: process.env.MASTER_KEY || '3DerL7gs4rHTdd2eaya6GjHGpP27tbtSpyqSlGMw', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://gnnhs.herokuapp.com/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    classNames: ["ClubChoice", "Alerts"] // List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
